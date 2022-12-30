7f0a08597dbddeaeb638b1a6e14293099bb69696
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2022-12-30 17:56:53 -0500

    remove comments

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -47,10 +47,3 @@ def stock_picker(days)
 
   [buy_index, sell_index]
 end
-
-# Test cases
-# p stock_picker([14, 3, 6, 9, 15, 8, 6]) # [1, 4]
-# p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10, 2, 500]) # [7, 10]
-# p stock_picker([11, 9, 6, 4, 1, 0]) # nil
-# p stock_picker([45, 24, 35, 31, 40, 38, 11]) # [1, 4]
-# p stock_picker([10, 11, 9, 10, 11]) # [2, 4]
